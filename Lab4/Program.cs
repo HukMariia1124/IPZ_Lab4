4bc0086de792e0eb3198dd8660eae497f1414c5c
Mariia Guk <mashahuk27@gmail.com>
2025-05-01 10:48:07 +0300

    Global Fix Commit

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,25 +1,154 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.RegularExpressions;
-
+ 
 namespace Lab4
 {
-    internal class Program
+    struct Float
     {
+        public string sign;
+        public string mantissa;
+        public string exp;
+
+        public Float(string sign, string mantissa, string exp)
+        {
+            this.sign = sign;
+            this.mantissa = mantissa;
+            this.exp = exp;
+        }
+        public Float(string str)
+        {
+            sign = str[0] == '-' ? "1" : "0";
+            str = str.Split(',', '.')[1];
+            var parts = str.Split('*').Select(x => x.Trim()).ToArray();
+            mantissa = parts[0];
+            exp = parts[1].Split('^').Select(x => x.Trim()).ToArray()[1];
+        }
+        public void InvertSign() => sign = sign == "1" ? "0" : "1";
+        public void ShiftMantissaRight(int shift)
+        {
+            mantissa = new string('0', shift) + mantissa;
+            mantissa = mantissa[..(mantissa.Length - shift)];
+        }
+        public void ShiftMantissaLeft(int shift)
+        {
+            mantissa = mantissa[shift..] + new string('0', shift);
+        }
+        public void AdditionalSwap()
+        {
+            if (sign == "1")
+            {
+                char[] additionalCode = mantissa.ToCharArray();
+                bool foundOne = false;
+                for (int i = additionalCode.Length - 1; i >= 0; i--)
+                {
+                    if (foundOne)
+                    {
+                        additionalCode[i] = additionalCode[i] == '0' ? '1' : '0';
+                    }
+                    if (additionalCode[i] == '1' && !foundOne)
+                    {
+                        foundOne = true;
+                    }
+                }
+                mantissa = new string(additionalCode);
+            }
+        }
+        public static Float operator +(Float a, Float b)
+        {
+            int maxLength = Math.Max(a.mantissa.Length, b.mantissa.Length);
+            a.mantissa = a.mantissa.PadLeft(maxLength, '0');
+            b.mantissa = b.mantissa.PadLeft(maxLength, '0');
+
+            char carry = '0';
+            char[] result = new char[maxLength + 1];
+
+            for (int i = maxLength - 1; i >= 0; i--)
+            {
+                int sum = (a.mantissa[i] - '0') + (b.mantissa[i] - '0') + (carry - '0');
+                if (sum >= 2)
+                {
+                    carry = '1';
+                    sum -= 2;
+                }
+                else
+                {
+                    carry = '0';
+                }
+                result[i + 1] = (sum == 0) ? '0' : '1';
+            }
+            result[0] = carry;
+
+            string resultSign = a.sign;
+            if (a.sign != b.sign)
+            {
+                resultSign = a.mantissa.CompareTo(b.mantissa) >= 0 ? a.sign : b.sign;
+            }
+            else if (carry == '1' && resultSign == "1")
+            {
+                resultSign = "10";
+            }
+            else if (carry == '1' && resultSign == "0")
+            {
+                resultSign = "1";
+            }
+
+            return new(resultSign, new string(result).TrimStart('0'), a.exp);
+        }
+        public void CheckNormalization()
+        {
+            if (sign.Length > 1)
+            {
+                ShiftMantissaRight(1);
+                exp = (Convert.ToInt32(exp, 2) + 1).BitLook();
+                sign = "1";
+            }
+            else if (sign[0] == mantissa[0])
+            {
+                int cnt = 0;
+                do
+                {
+                    cnt++;
+                    ShiftMantissaLeft(1);
+                    exp = (Convert.ToInt32(exp, 2) - 1).BitLook();
+                }
+                while (cnt<15 && (sign=="1"? mantissa[0] != '0': mantissa[0] != '1'));
+            }
+        }
+        public string Result()
+        {
+            string res = "";
+            if (sign == "1") res = "-";
+            res += "0," + mantissa + " * 2^" + exp;
+            return res;
+        }
+        public override string ToString() => $"{sign}|{mantissa}|{exp}";
+    }
+    static class Program
+    {
+        public static string BitLook(this int a)
+        {
+            StringBuilder sb = new();
+            foreach (var i in Enumerable.Range(0, 15).Reverse())
+                sb.Append((a & 0x1 << i) == 0b0 ? "0" : "1");
+
+            return sb.ToString().TrimStart('0');
+        }
+
         static void Main()
         {
-            Console.InputEncoding = System.Text.Encoding.Unicode;
-            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
             //Ввід
-            string num1, num2;
-            Input(out num1, out num2);
-
-
+            Input(out string data1, out string data2);
+            Float num1 = new(data1);
+            Float num2 = new(data2);
+            num2.InvertSign();
+            
             //Перевід в прямий код
-            num1 = Transform(num1);
-            if (num2[0] == '-') num2 = Transform(num2[1..]);
-            else num2 = Transform('-' + num2);
             Console.WriteLine("КРОК 0 (Запис в прямому коді):");
             Console.WriteLine($"Перше число в прямому коді: {num1}");
             Console.WriteLine($"Друге число в прямому коді: {num2} (Зміна знаку для виконання віднімання)");
@@ -27,73 +156,42 @@ namespace Lab4
 
 
             Console.WriteLine("КРОК 1 (Урівноваження порядків доданків):");
-            (int, int) power = PowerDifference(num1[18..], num2[10..]);
-            Console.WriteLine($"Різниця між степенями: {power.Item2}. Менший степінь в числі #{power.Item1}");
-            if (power.Item1 == 1) //Зсув елементнів в першому числі.
-            {
-                int temp = 17 - power.Item2;
-                num1 = num1[0..2] + new string('0', power.Item2) + num1[2..temp] + num2[9..];
-                Console.WriteLine($"Зсуваємо перше число на {power.Item2}: {num1}");
-            }
-            else //В другому числі.
-            {
-                int temp = 9 - power.Item2;
-                num2 = num2[0..2] + new string('0', power.Item2) + num2[2..temp] + num1[17..];
-                Console.WriteLine($"Зсуваємо друге число на {power.Item2}: {num2}");
-            }
+            int diff = Math.Abs(Convert.ToInt32(num1.exp,2) - Convert.ToInt32(num2.exp, 2));
+            if (Convert.ToInt32(num1.exp, 2) > Convert.ToInt32(num2.exp, 2))
+                num2.ShiftMantissaRight(diff);
+            else num1.ShiftMantissaRight(diff);
+            string max = Math.Max(Convert.ToInt32(num1.exp, 2), Convert.ToInt32(num2.exp, 2)).BitLook();
+            num1.exp = max;
+            num2.exp = max;
+            Console.WriteLine($"Перше число після урівноваження порядків: {num1}");
+            Console.WriteLine($"Друге число після урівноваження порядків: {num2}");
 
 
 
             Console.WriteLine("КРОК 2 (Перетворення мантис чисел в додатковий код):");
-            num1 = ConvertCodes(num1[0..1] + num1[2..17]) + num1[17..];
-            num2 = ConvertCodes(num2[0..1] + num2[2..9]) + num2[9..];
-            num1 = num1[0..1] + "|" + num1[1..];
-            num2 = num2[0..1] + "|" + num2[1..];
+            num1.AdditionalSwap();
+            num2.AdditionalSwap();
             Console.WriteLine($"Перше число в додатковому коді: {num1}");
             Console.WriteLine($"Друге число в додатковому коді: {num2}");
 
 
 
-            Console.WriteLine("КРОК 3 (Додавання мантис):"); 
-            string sum = SumBinary(num1[0..1] + num1[2..17], num2[0..1] + new string('0', 8) + num2[2..9]);
-            if (sum.Length == 16) sum = sum[0..1] + "|" + sum[1..] + num2[9..];
-            else sum = sum[0..2] + "|" + sum[2..] + num2[9..];
-            Console.WriteLine($"Різниця цих чисел в додатковому коді: {sum}");
+            Console.WriteLine("КРОК 3 (Додавання мантис):");
+            Float sum = num1 + num2;
+            Console.WriteLine($"Сума цих чисел в додатковому коді: {sum}");
 
 
 
             Console.WriteLine("КРОК 4 (Денормалізація результату):");
-            string power2 = num2[9..];
-            if (sum.IndexOf('|') == 2)
-            {
-                power2 = "|" + SumBinary(num2[10..], "1");
-                sum = sum[0..1] + "|" + sum[1..2] + sum[3..17] + power2;
-                Console.WriteLine($"Має місце денормалізація результату вліво, тому зсуваємо вправо на 1: {sum}");
-            }
-            else if (sum[0] == sum[2])
-            {
-                int cnt = 0;
-                char sign='1';
-                if (sum[0] == '1') sign = '0';
-                Console.WriteLine($"Має місце денормалізація результату вправо, тому зсуваємо вліво, поки не знайдемо {sign}:");
-                while (sum[2] != sign && cnt<7)
-                {
-                    cnt++;
-                    power2 = "|" + SumBinary(num2[10..], "-1");
-                    sum = sum[2..3] + "|" + sum[3..17] + '0' + power2;
-                    Console.WriteLine(sum);
-                }
-            }
-            else Console.WriteLine("Уже нормалізований.");
+            //sum.CheckNormalization();
+            Console.WriteLine($"Результат після нормалізування: {sum}");
 
 
 
             Console.WriteLine("КРОК 5 (Подання результату):");
-            sum = ConvertCodes(sum[0..1] + sum[2..17]);
-            sum = sum[0..1] + "|" + sum[1..] + power2;
-            Console.WriteLine($"Різниця цих чисел в прямому коді: {sum}");
-            sum = TransformResult(sum);
-            Console.WriteLine($"Результат: {sum}");
+            sum.AdditionalSwap();
+            Console.WriteLine($"Результат в прямому коді: {sum}");
+            Console.WriteLine($"Результат: {sum.Result()}");
         }
 
         private static void Input(out string num1, out string num2)
@@ -116,83 +214,6 @@ namespace Lab4
                 else Console.WriteLine("Помилка! Спробуйте ще раз.");
             }
             while (true);
-        }
-
-        static string Transform(string num)
-        {
-            StringBuilder result = new StringBuilder();
-            int i = 0;
-            result.Append(num[i] == '-' ? "1|" : "0|");
-            i += (num[i] == '-') ? 3 : 2;
-            while (num[i] != ' ') result.Append(num[i++]);
-            i += 5;
-            result.Append("|" + num[i..]);
-            return result.ToString();
-        }
-        static string TransformResult(string num)
-        {
-            StringBuilder result = new StringBuilder();
-            int i = 0;
-            result.Append(num[i] == '1' ? "-0," : "0,");
-            i += 2;
-            while (num[i] != '|') result.Append(num[i++]);
-            i++;
-            result.Append(" * 2^" + num[i..]);
-            return result.ToString();
-        }
-        static (int, int) PowerDifference(string num1, string num2)
-        {
-            int temp1 = Convert.ToInt32(num1, 2);
-            int temp2 = Convert.ToInt32(num2, 2);
-
-            int diff = Math.Abs(temp1 - temp2);
-
-            return ((temp1 < temp2) ? 1 : 2, diff);
-        }
-        static string ConvertCodes(string directCode)
-        {
-            if (directCode[0] == '0') return directCode;
-            char[] additionalCode = directCode.ToCharArray();
-            bool foundOne = false;
-            for (int i = additionalCode.Length - 1; i > 0; i--)
-            {
-                if (foundOne)
-                {
-                    additionalCode[i] = additionalCode[i] == '0' ? '1' : '0';
-                }
-                if (additionalCode[i] == '1' && !foundOne)
-                {
-                    foundOne = true;
-                }
-            }
-            return new string(additionalCode);
-        }
-        static string SumBinary(string num1, string num2)
-        {
-            int maxLength = Math.Max(num1.Length, num2.Length);
-            num1 = num1.PadLeft(maxLength, '0');
-            num2 = num2.PadLeft(maxLength, '0');
-
-            char remember = '0';
-            char[] result = new char[maxLength];
-
-            for (int i = maxLength - 1; i >= 0; i--)
-            {
-                int sum = (num1[i] - '0') + (num2[i] - '0') + (remember - '0');
-                if (sum >= 2)
-                {
-                    remember = '1';
-                    sum -= 2;
-                }
-                else
-                {
-                    remember = '0';
-                }
-                result[i] = (sum == 0) ? '0' : '1';
-            }
-
-            if (remember == '1') return "1" + new string(result);
-            else return new string(result);
         }
     }
 }
